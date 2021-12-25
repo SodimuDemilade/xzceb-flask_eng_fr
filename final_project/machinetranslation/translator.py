3f99390524807b6fe78e1b271e4dfa27820ac1a6
SodimuDemilade <66140481+SodimuDemilade@users.noreply.github.com>
2021-12-25 08:26:16 -0800

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -20,13 +20,12 @@ def english_to_french(english_text):
     translation = language_translator.translate(
         text=english_text,
         model_id='en-fr').get_result()
-    french_text = translation
+    french_text = translation['translations'][0]['translation']
     return french_text
 
 def french_to_english(french_text):
     translation = language_translator.translate(
         text=french_text,
         model_id='fr-en').get_result()
-    english_text = translation
+    english_text = translation['translations'][0]['translation']
     return english_text
-    
